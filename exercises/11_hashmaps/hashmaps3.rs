387b2b518b852e407d6d59203fa69cc75bfbdd5a
hubertshelley <hubertshelley@163.com>
2024-07-23 08:34:36 +0800

    进行中

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,32 @@ fn build_scores_table(results: &str) -> HashMap<&str, Team> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        if !scores.contains_key(&team_1_name) {
+            scores.insert(
+                team_1_name.clone(),
+                Team {
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score,
+                },
+            );
+        } else {
+            let team = scores.get_mut(&team_1_name).unwrap();
+            team.goals_scored += team_1_score;
+            team.goals_conceded += team_2_score;
+        }
+        if !scores.contains_key(&team_2_name) {
+            scores.insert(
+                team_2_name.clone(),
+                Team {
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score,
+                },
+            );
+        } else {
+            let team = scores.get_mut(&team_2_name).unwrap();
+            team.goals_scored += team_2_score;
+            team.goals_conceded += team_1_score;
+        }
     }
 
     scores
